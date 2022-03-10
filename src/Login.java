5a3993542c9262ddcb494a98974d01ad95f75f8a
Mushurisen <72408654+Mushurisen@users.noreply.github.com>
2022-03-10 14:52:42 -0500

    login

--- src/Login.java
+++ src/Login.java
@@ -1,2 +1,19 @@
+import javax.swing.*;
+
 public class Login {
+    public static void main(String arg[])
+    {
+        try
+        {
+            //create instance of the CreateLoginForm
+            CreateLoginForm form = new CreateLoginForm();
+            form.setSize(300,100);  //set size of the frame
+            form.setVisible(true);  //make form visible to the user
+        }
+        catch(Exception e)
+        {
+            //handle exception
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
 }
