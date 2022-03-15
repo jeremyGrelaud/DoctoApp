4029c044619c79ed5b036908df65e9888be2f972
jeremyGrelaud <jeremy.grelaud@efrei.net>
2022-03-15 16:32:53 -0400

    Update HelpWindow.java
    fixed display

--- src/HelpWindow.java
+++ src/HelpWindow.java
@@ -8,12 +8,12 @@ public class HelpWindow implements ActionListener {
     private JFrame frame = new JFrame();
     private JButton button_return_to_menu;
     private int id_user;
-    private JLabel content;
+
+    private JPanel panel = new JPanel();
     HelpWindow(int id_user) {
         this.id_user=id_user;
         //creating a button allowing to go back to the main menu
-        
-
+        Display_help(id_user);
     }
 
     @Override
@@ -27,26 +27,46 @@ public class HelpWindow implements ActionListener {
 
     private void Display_help(int id_user)
     {
-        this.content = new JLabel("This is the help page of Doctoapp !\n\n" + 
-        "Doctoapp allow you to stay in contact with a guardian for your treatment and with doctors.\n" +
-        "Go to Treatment to see your treatment and check that you take it, if you forgot, a notification will be sent to your guardian.\n" +
-        "You can also manage your schedule of doctor appoinment on the section my appointments" +
-        "Finaly you can access to your guardian's informations by the guardian section" +
-        "If you have any suggestion or feedback, please contact us at jeremy.grelot@efrei.net");
+        GridLayout layout = new GridLayout(8,1);
+        panel.setLayout(layout);
+        panel.setBounds(0,300,800,800);
+
+        JLabel content1 = new JLabel("This is the help page of Doctoapp ! Doctoapp allow you to stay in contact with",SwingConstants.CENTER);
+        JLabel content2 = new JLabel("a guardian for your treatment and with doctors. Go to Treatment to see your",SwingConstants.CENTER);
+        JLabel content3 = new JLabel("treatment and check that you take it, if you forgot, a notification will",SwingConstants.CENTER);
+        JLabel content4 = new JLabel("be sent to your guardian. You can also manage your schedule of doctor ",SwingConstants.CENTER);
+        JLabel content5 = new JLabel("appoinment on the section my appointments Finaly you can access to your ",SwingConstants.CENTER);
+        JLabel content6 = new JLabel( "guardian's informations by the guardian section If you have any suggestion" ,SwingConstants.CENTER);
+        JLabel content7 = new JLabel("or feedback, please contact us at jeremy.grelot@efrei.net",SwingConstants.CENTER);
+
+        content1.setFont(new Font("Verdana",Font.PLAIN,18));
+        content2.setFont(new Font("Verdana",Font.PLAIN,18));
+        content3.setFont(new Font("Verdana",Font.PLAIN,18));
+        content4.setFont(new Font("Verdana",Font.PLAIN,18));
+        content5.setFont(new Font("Verdana",Font.PLAIN,18));
+        content6.setFont(new Font("Verdana",Font.PLAIN,18));
+        content7.setFont(new Font("Verdana",Font.PLAIN,18));
+
+
         button_return_to_menu = new JButton("Return to menu");
-        button_return_to_menu.setSize(200, 80);
-        button_return_to_menu.setLocation(300, 50);
         button_return_to_menu.addActionListener(this);
 
-        frame.add(this.content);
-        frame.add(button_return_to_menu);
-        frame.setLayout(null);
-        frame.setVisible(true);
+        panel.add(button_return_to_menu);
+        panel.add(content1);
+        panel.add(content2);
+        panel.add(content3);
+        panel.add(content4);
+        panel.add(content5);
+        panel.add(content6);
+        panel.add(content7);
 
+
+        frame.add(panel);
         frame.setSize(800, 800);
         frame.setTitle("Help");
         frame.setBackground(Color.GRAY);
         frame.setResizable(false);
+        frame.setVisible(true);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //for the exit cross
     }
